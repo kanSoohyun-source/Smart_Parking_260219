644e3e6f771082e5189c8dd447969db0b7a4ba03
한정수 <wjdtn1642@naver.com>
2026-02-10 14:31:45 +0900

    change 0210
    입차시 대시보드 화면에서 주차 현황이 실시간 반영될 수 있게 하였습니다.

--- src/main/java/org/example/smart_parking_260219/dao/ParkingDAO.java
+++ src/main/java/org/example/smart_parking_260219/dao/ParkingDAO.java
@@ -2,10 +2,13 @@ package org.example.smart_parking_260219.dao;
 
 import org.example.smart_parking_260219.vo.ParkingVO;
 
+import java.util.List;
+
 public interface ParkingDAO {
     void insertParking(ParkingVO parkingVO);                // 입차된 시간
     ParkingVO selectParkingByLast4(String last4);   // 차번호 뒷자리 차량 찾기
     ParkingVO selectParkingByCarNum(String carNum);   //
     void updateParking(ParkingVO parkingVO);               // 출차된 시간
     ParkingVO selectParkingByParkingId(int parkingId);      // 프라이머리키 기준 차량 조회
+    List<ParkingVO> selectAllParking();
 }
